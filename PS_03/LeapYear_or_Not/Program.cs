3b49aaf5e5dde165b7aa5562a5322a3618b0de8b
John Brence Condesa <condesajohnbrence@gmail.com>
2024-09-20 00:38:40 +0800

    done

---
+++ PS_03/LeapYear_or_Not/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeapYear_or_not
+{
+    class Program
+    //function that determines whether a given year is a leap year or not.
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a Year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            if (LeapYear(year))
+            {
+                Console.Write($"{year} is a leap year.");
+            }
+
+            else
+            {
+                Console.Write($"{year} is not a leap year.");
+            }
+        }
+
+        static bool LeapYear(int year)
+        {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
